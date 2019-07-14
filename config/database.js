79aa33ad2bd364a5032c582fd92cef102316555d
Tachcha Wongsapan <tadcha5000@gmail.com>
2019-07-14 11:54:13 +0700

    before change

---
+++ config/database.js
@@ -0,0 +1,11 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'todo collation'
+});
+
+connection.connect();
+
+module.exports = connection
