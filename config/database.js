e96a383b457b65fd8d9951d228ec5f8f7744f6b5
Tachcha Wongsapan <tadcha5000@gmail.com>
2019-07-14 16:10:54 +0700

    delete

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'root',
-  database : 'todo collation'
+  database : 'todo'
 });
 
 connection.connect();
